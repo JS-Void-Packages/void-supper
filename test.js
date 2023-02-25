76d3f54d7655a7e065f37ca45a81b92980521798
Mrthomas20121 <thomas77300@gmail.com>
2023-02-25 06:42:45 +0100

    Tests

---
+++ test.js
@@ -0,0 +1,16 @@
+const { upper, sUpper, aUpper } = require('./index');
+
+let text = 'hello there';
+let text1 = 'insert_text_here';
+
+// convert hello there into Hello There
+console.log(upper(text));
+
+// convert insert_text_here into Insert Text Here
+console.log(upper(text1));
+
+// convert hello there into Hello there
+console.log(sUpper(text));
+
+// convert hello there into hello There
+console.log(aUpper(text, 1));
