c73d4391e95b3068b0e259617e6e40a371c695bb
Mrthomas20121 <thomas77300@gmail.com>
2023-02-25 06:42:54 +0100

    Typing

---
+++ index.d.ts
@@ -0,0 +1,17 @@
+/**
+ * Make the first letter Uppercase and make the rest lowercase.
+ * @param text 
+ */
+export function upper(text: string): string;
+
+/**
+ * make the first letter of the first word in the sentence Uppercase.
+ * @param text 
+ */
+export function sUpper(text: string): string;
+
+/**
+ * make a word in the sentence Uppercase.
+ * @param text 
+ */
+export function aUpper(text: string): string;
